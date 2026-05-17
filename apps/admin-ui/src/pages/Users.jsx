dd9724b7fd0bcb3f465c6d557e8851e829798688
elisa-widjaja <elisa.widjaja@gmail.com>
2026-05-16 22:09:33 -0700

    style(admin-ui): relabel Users 'Acc' column to 'Friends' (it's total friend count)

--- apps/admin-ui/src/pages/Users.jsx
+++ apps/admin-ui/src/pages/Users.jsx
@@ -65,7 +65,7 @@ export default function Users() {
       cell: (i) => new Date(i.getValue()).toLocaleDateString() },
     { accessorKey: 'recipe_count', header: 'Recipes', cell: (i) => i.getValue() },
     { accessorKey: 'invites_sent', header: 'Sent', cell: (i) => i.getValue() },
-    { accessorKey: 'invites_accepted', header: 'Acc', cell: (i) => i.getValue() },
+    { accessorKey: 'invites_accepted', header: 'Friends', cell: (i) => i.getValue() },
     {
       id: 'active',
       header: 'Active',
